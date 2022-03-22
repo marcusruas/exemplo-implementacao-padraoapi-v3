cd8f9c72099c8c8560eba89e8b3f4c28ee024b6f
marcusruas <marcus.ruas@hotmail.com>
2022-03-22 01:27:35 -0300

    Adicionado testes de integração e unitários

---
+++ Tests/Livros/IntegrationTests/LivroControllerIntegrationTests.cs
@@ -0,0 +1,43 @@
+﻿using Api;
+using Domain.Livros.Entities;
+using Infrastructure.DBContexts;
+using MandradeFrameworks.Retornos.Models;
+using MandradeFrameworks.SharedKernel.Models;
+using MandradeFrameworks.Tests.Integration;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Livros.IntegrationTests
+{
+    public class LivrosControllerIntegrationTests : IntegrationTestsController<Startup, LivrosContext>
+    {
+        public LivrosControllerIntegrationTests(ApplicationTests<Startup, LivrosContext> app)
+        : base(app, SetupDb()) { }
+
+        [Fact]
+        public async Task ListarTodosLivros_DeveRetornarSucesso()
+        {
+            var urlRequest = $"{UrlBase}/1/10";
+            var request = await Cliente.GetAsync(urlRequest);
+            var resposta = await DesserializarResposta<ListaPaginadaModel<Livro>>(request);
+
+            resposta.Sucesso.ShouldBeTrue();
+            resposta.Dados.Itens.ShouldNotBeEmpty();
+        }
+
+        protected static Func<LivrosContext, Task> SetupDb()
+        {
+            return async context =>
+            {
+                await context.Livros.AddAsync(new Livro("IT - A Coisa", "Livro sobre palhaço"));
+                await context.SaveChangesAsync();
+            };
+        }
+
+        protected override string UrlControllerBase() => "livros";
+    }
+}
---
+++ Tests/Livros/UnitTests/Domain/LivroUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Livros.Entities;
+using MandradeFrameworks.Tests.UnitTests;
+using Shouldly;
+using Xunit;
+
+namespace Tests.Livros.UnitTests.Domain
+{
+    public class LivroUnitTests : UnitTestsClass
+    {
+        [Fact]
+        public void Livro_Instanciacao_DeveSerIgual()
+        {
+            var nome = _faker.Random.String(0, 100);
+            var descricao = _faker.Random.String(0, 500);
+
+            var livro = new Livro(nome, descricao);
+
+            livro.Nome.ShouldBe(nome);
+            livro.Descricao.ShouldBe(descricao);
+        }
+
+        [Fact]
+        public void Livro_InstanciacaoSemDescricao_DeveSerIgual()
+        {
+            var nome = _faker.Random.String(0, 100);
+
+            var livro = new Livro(nome, null);
+
+            livro.Nome.ShouldBe(nome);
+            livro.Descricao.ShouldBeNull();
+        }
+    }
+}
